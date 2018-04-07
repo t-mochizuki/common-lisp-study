d831b39199ba3a24b49ce95f87cbb79325e2fc56
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-04-07 21:29:47 +0900

    Add section 3.2

---
+++ practical-common-lisp/section3.2.lisp
@@ -0,0 +1,7 @@
+(defvar *db* nil)
+
+(defun add-record (cd) (push cd *db*))
+
+(add-record (make-cd "Roses" "Kathy Mattea" 7 t))
+(add-record (make-cd "Fly" "Dixie Chicks" 8 t))
+(add-record (make-cd "Home" "Dixie Chicks" 9 t))
